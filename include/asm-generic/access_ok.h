6a933a4e6c1da8e821d684a1d43fa460c325f1c4
Nick Wanninger <nickwanninger@gmail.com>
2024-09-19 16:08:26 -0500

    fix warnings

--- include/asm-generic/access_ok.h
+++ include/asm-generic/access_ok.h
@@ -30,10 +30,10 @@
  */
 static inline int __access_ok(const void __user *ptr, unsigned long size)
 {
-    return true;
     // ptr = alaska_translate(ptr);
 	unsigned long limit = TASK_SIZE_MAX;
 	unsigned long addr = (unsigned long)ptr;
+  return true;
 
 	if (IS_ENABLED(CONFIG_ALTERNATE_USER_ADDRESS_SPACE) ||
 	    !IS_ENABLED(CONFIG_MMU))
--- include/linux/alaska.h
+++ include/linux/alaska.h
@@ -14,33 +14,32 @@
 
 #define HANDLE_OFFSET_SIZE 32
 
-typedef unsigned long (*copy_from_user_fn_t)(void *, const void *, unsigned long);
+typedef unsigned long (*copy_from_user_fn_t)(void *, const void *,
+					     unsigned long);
 
 static inline void *__alaska_translate(void *vaddr, copy_from_user_fn_t copy)
 {
 	uint64_t addr;
 	void *handle_table_addr;
 	uint64_t hte;
+	uint64_t result;
 
 	addr = (uint64_t)vaddr;
 	// Handle check
 	if ((addr >> 62) != 0b10)
 		return vaddr;
 
-
 	handle_table_addr = (void *)(addr >> (HANDLE_OFFSET_SIZE - 3));
 	// EEP
-	copy((void *)&hte, (const void __user *)handle_table_addr,
-		       sizeof(hte));
+	copy((void *)&hte, (const void __user *)handle_table_addr, sizeof(hte));
 
-	uint64_t result =
-		(uint64_t)((uint64_t)hte +
-			   (addr & ((1LU << HANDLE_OFFSET_SIZE) - 1)));
+	result = (uint64_t)((uint64_t)hte +
+			    (addr & ((1LU << HANDLE_OFFSET_SIZE) - 1)));
 
 	// printk(KERN_INFO "alaska_translate(%llx) -> %llx\n", addr, result);
 
 	return (void *)result;
 }
 
-
-#define alaska_translate(x) (__typeof__(x))__alaska_translate((void*)(x), copy_from_user)
+#define alaska_translate(x) \
+	(__typeof__(x))__alaska_translate((void *)(x), copy_from_user)
