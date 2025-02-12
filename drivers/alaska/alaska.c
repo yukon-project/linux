d98264d404ee5154ebc999c18546b6d7d8799e4e
Nick Wanninger <nickwanninger@gmail.com>
2025-02-12 16:59:09 -0600

    print the handle table size in bytes

--- drivers/alaska/alaska.c
+++ drivers/alaska/alaska.c
@@ -19,8 +19,8 @@ static int my_mmap(struct file *filp, struct vm_area_struct *vma)
 {
 	unsigned long size = vma->vm_end - vma->vm_start;
 	void *handle_table_ptr = NULL;
-	long bytes;
 	int order = 9;
+  unsigned long bytes = (1LU << order) * 4096;
 
 	if (size > MEM_SIZE)
 		return -EINVAL;
@@ -28,7 +28,7 @@ static int my_mmap(struct file *filp, struct vm_area_struct *vma)
 	if (handle_table_page == 0) {
 		handle_table_page =
 			alloc_pages(GFP_KERNEL, order /* order 9 is 2mb */);
-		bytes = (1LU << order) * 4096;
+
 
 	  __asm__ volatile("csrw 0xc2, %0" ::"rK"(__pa(page_address(handle_table_page)))
 			 : "memory");
@@ -37,7 +37,7 @@ static int my_mmap(struct file *filp, struct vm_area_struct *vma)
 
 	handle_table_ptr = page_address(handle_table_page);
 
-	pr_info("handle table at 0x%llx\n", __pa(handle_table_ptr));
+	pr_info("handle table at 0x%llx, %lu bytes\n", __pa(handle_table_ptr), bytes);
 
 	return remap_pfn_range(vma, vma->vm_start,
 			       __pa(handle_table_ptr) >> PAGE_SHIFT, size,
