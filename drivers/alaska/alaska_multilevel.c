4b893581822e2a04ed4d88a36791f37194bfdc40
Nick Wanninger <nickwanninger@gmail.com>
2025-03-12 14:42:59 -0500

    allow dumping within the kernel

--- drivers/alaska/alaska_multilevel.c
+++ drivers/alaska/alaska_multilevel.c
@@ -13,6 +13,22 @@ static dev_t dev_num;
 static struct cdev my_cdev;
 static struct class *my_class;
 
+#define CSR_HTBASE 0xc2
+#define CSR_HTDUMP 0xc3
+#define CSR_HTINVAL 0xc4
+
+#define write_csr(reg, val)                                                \
+	({                                                                 \
+		asm volatile("csrw %0, %1" ::"i"(reg), "rK"((uint64_t)val) \
+			     : "memory");                                  \
+	})
+
+#define read_csr(csr, val)                 \
+	__asm__ __volatile__("csrr %0, %1" \
+			     : "=r"(val)   \
+			     : "n"(csr)    \
+			     : /* clobbers: none */);
+
 /**
  * the handle table is a two-level table with each level being 2mb in total (or
  * 18 bits worth of 8 byte values). The application indexes into it using
@@ -77,7 +93,6 @@ static void *alaska_allocate_ht(void)
 	return p;
 }
 
-
 static void *alaska_get_ht1(struct alaska_handle_table *ht, unsigned index)
 {
 	if (index > HT_ENTRIES) {
@@ -99,13 +114,12 @@ static void *alaska_get_ht1(struct alaska_handle_table *ht, unsigned index)
 				 : "memory");
 	}
 
-  // for (i = 0; i < ht->length; i++) {
-  //   printk("ht0[%2d]: %zx\n", i, ht->ht0[i]);
-  // }
+	// for (i = 0; i < ht->length; i++) {
+	//   printk("ht0[%2d]: %zx\n", i, ht->ht0[i]);
+	// }
 
 	return ht->ht0[index];
 }
-
 
 static void alaska_handle_table_init(struct alaska_handle_table *ht)
 {
@@ -114,8 +128,8 @@ static void alaska_handle_table_init(struct alaska_handle_table *ht)
 	__asm__ volatile("csrw 0xc2, %0" ::"rK"(__pa(ht->ht0)) : "memory");
 	__asm__ volatile("csrw 0xc5, %0" ::"rK"(ht->length * HT_ENTRIES)
 			 : "memory");
-  // Initialize 16 entries
-  alaska_get_ht1(ht, 16);
+	// Initialize 16 entries
+	alaska_get_ht1(ht, 16);
 }
 
 // Currently, we only support a single handle table in the system.
@@ -129,7 +143,7 @@ static vm_fault_t alaska_vma_fault(struct vm_fault *vmf)
 	unsigned long index = byte_off / HT_SIZE;
 	unsigned long bytes_into_ht1 = byte_off % HT_SIZE;
 	unsigned long pfn = 0;
-  unsigned long map_address = vmf->address & ~0xfff;
+	unsigned long map_address = vmf->address & ~0xfff;
 	struct page *page;
 
 	// printk("fault address:        0x%lx\n", vmf->address);
@@ -157,9 +171,9 @@ static vm_fault_t alaska_vma_fault(struct vm_fault *vmf)
 	page = pfn_to_page((unsigned long)(ht1 + bytes_into_ht1) >> PAGE_SHIFT);
 	pfn = page_to_pfn(page);
 
-  // printk("remap_pfn_range %16zx %16zx %16zx %x", map_address, pfn, 4096, vmf->vma->vm_page_prot);
-	int err = remap_pfn_range(vmf->vma, map_address, pfn,
-				  4096, vmf->vma->vm_page_prot);
+	// printk("remap_pfn_range %16zx %16zx %16zx %x", map_address, pfn, 4096, vmf->vma->vm_page_prot);
+	int err = remap_pfn_range(vmf->vma, map_address, pfn, 4096,
+				  vmf->vma->vm_page_prot);
 	if (err) {
 		printk("remap_pfn_range failed\n");
 		return err;
@@ -177,7 +191,7 @@ static struct vm_operations_struct alaska_vm_ops = {
 static int alaska_handle_table_mmap(struct file *filp,
 				    struct vm_area_struct *vma)
 {
-  printk("alaska mmap %zx - %zx\n", vma->vm_start, vma->vm_end);
+	printk("alaska mmap %zx - %zx\n", vma->vm_start, vma->vm_end);
 	unsigned long region_size = vma->vm_end - vma->vm_start;
 	unsigned long entries = region_size / HT_SIZE;
 	unsigned long i;
@@ -185,10 +199,65 @@ static int alaska_handle_table_mmap(struct file *filp,
 	return 0;
 }
 
+static struct page *alaska_dump_page = NULL;
+
+// This function is called when the runtime calls read() on our device file.
+// The main goal of this is to
+static ssize_t alaska_read(struct file *filp, char __user *buf, size_t len,
+			   loff_t *offset)
+{
+	int order = 2;
+	ssize_t bytes_read = 0;
+	void *alaska_dump_page_virtual, *alaska_dump_page_physical;
+	unsigned long htbase;
+	volatile int dump_reg = 0;
+
+	read_csr(CSR_HTBASE, htbase);
+	if (htbase == 0) {
+		printk("handle table not initialized\n");
+		return -EINVAL;
+	}
+
+	if (alaska_dump_page == NULL) {
+		// allocate 2 pages for the dump page
+		// they are physically contiguous
+		alaska_dump_page = alloc_pages(GFP_KERNEL, order);
+		if (alaska_dump_page == NULL) {
+			printk("failed to allocate page\n");
+			return -ENOMEM;
+		}
+	}
+
+	alaska_dump_page_virtual = page_address(alaska_dump_page);
+	alaska_dump_page_physical = __pa(alaska_dump_page_virtual);
+
+	write_csr(CSR_HTDUMP, (uint64_t)alaska_dump_page_physical);
+	do {
+		read_csr(CSR_HTDUMP, dump_reg);
+	} while (dump_reg);
+
+	// TODO: dump
+
+	if (len > order * 4096) {
+		len = order * 4096;
+	}
+
+	// Copy data from kernel space to user space
+	if (copy_to_user(buf, alaska_dump_page_virtual, len))
+		return -EFAULT;
+
+	// Update the offset and the number of bytes read
+	bytes_read = len;
+	printk("read %zu\n", bytes_read);
+
+	return bytes_read;
+}
+
 /* File operations */
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.mmap = alaska_handle_table_mmap,
+	.read = alaska_read,
 };
 
 /* Module Init */
